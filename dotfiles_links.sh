462d542977624fc6dde6cd4ff7cf0a16bcb1caa8
venguru <kin.sh0717@gmail.com>
2019-02-27 16:21:08 +0900

    vimカラースキーマの自動インストールを追加

--- dotfiles_links.sh
+++ dotfiles_links.sh
@@ -1,3 +1,12 @@
+dir="../.vim/colors"
+vim_color=$dir"/hybrid.vim"
+if [ ! -e $vim_color ];then
+    if [ ! -e $dir ]; then
+        makedirs $dir
+    fi
+    wget https://raw.githubusercontent.com/w0ng/vim-hybrid/master/colors/hybrid.vim -P $dir
+fi
+
 ln -sf ~/dotfiles/vimrc ~/.vimrc
 ln -sf ~/dotfiles/bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/bashrc ~/.bashrc
